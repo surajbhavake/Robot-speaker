69f09da24e26b5e79f472023a24f971b11aa8af7
surajbhavake <surajbhavake2@gmail.com>
2025-09-08 19:49:03 +0530

    In future we can take voice from a client and covert into other voices

---
+++ Robot.py
@@ -0,0 +1,17 @@
+#as I am using ubuntu I have install sudo apt-get install espeak for using it 
+#If you are using windows it gives you by default say command you can use that
+import os
+
+while True:
+
+    x = input("Enter the text you want to convert to speech for your robot(press q for exit):")
+    
+    
+    if x == 'q':
+        os.system("espeak 'Goodbye'")
+        break
+  
+    command = f"espeak '{x}'"
+    os.system(command)
+
+
